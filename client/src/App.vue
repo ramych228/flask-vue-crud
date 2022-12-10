33218bbf5c6661dce1c2259dcf59de1ec635f067
Ivanov Svyatoslav <si.ivanov1212@gmail.com>
2022-12-10 15:46:41 +0300

    Added PrimeButton

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <PrimeButton text="Click" />
     <router-view/>
   </div>
 </template>
 
 <script>
+import PrimeButton from "./components/default_components/PrimeButton";
 export default {
   name: 'App',
+  components: {PrimeButton},
 };
 </script>
 
