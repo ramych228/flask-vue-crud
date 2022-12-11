660e772c3ff57133cd0d90b6807ab6762434faae
VlasovDmitriy <vlasov.dmitriy.dv1@gmail.com>
2022-12-11 15:22:05 +0300

    client/index.html

--- client/src/components/projects_page/ProjectCard.vue
+++ client/src/components/projects_page/ProjectCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
     <div class="header">
-      <h3>{{project.name}}</h3>
-      <p>></p>
+      <h3 class="text_name">{{project.name}}</h3>
+      <p class="arrow">⟩</p>
     </div>
-    <p>{{project.brief}}</p>
+    <p class="text_brief">{{project.brief}}</p>
   </div>
 </template>
 
@@ -30,18 +30,42 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  width: 300px;
-  height: 300px;
+  width: 250px;
+  height: 150px;
   padding: 10px;
   gap: 10px;
   border-radius: 10px;
-  border: 1px solid white;
+  background-repeat: no-repeat;
+  background-size: 200%;
+  transition: all 1s;
+  font-family: 'Montserrat', sans-serif;
+  padding-left: 25px;
+  padding-right: 25px;
+  filter: brightness(75%);
 }
+.container:hover {
+  transition: all 0.5s;
+  background-size: 210%; 
+}
+
 .header {
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   width: 100%;
+}
+
+.text_name {
+  font-family: 'Montserrat', sans-serif;
+}
+.text_brief {
+  font-family: 'Montserrat', sans-serif;
+}
+.arrow{
+  font-family: 'Montserrat', sans-serif;
+  font-size: 2em;
+  margin-top: 0;
+  margin-bottom: 0;
 }
 </style>
